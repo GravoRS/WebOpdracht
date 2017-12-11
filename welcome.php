85bc822b2509baaefc25b454eafc521e04e82504
Mats010 <34424857+Mats010@users.noreply.github.com>
2017-12-11 21:39:01 +0100

    Add files via upload

--- welcome.php
+++ welcome.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="mystyle.css">
-<title>
-Contact
-</title>
 </head>
 <body>
 <ul class="nav">
@@ -19,11 +15,10 @@ Contact
 
 </ul>
 
-<form action="welcome.php" method="post">
-Name: <input type="text" name="name"><br> <br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit" value="Contact Us">
-</form>
+Beste <?php echo $_POST["name"]; ?>, bedankt dat u contact heeft opgenomen met ons.<br>
+We zullen u binnenkort via <?php echo $_POST["email"]; ?> benaderen. <br><br>
+Met vriendelijke groet, <br><br>
+Teamnaam. <br><br>
 
 <p>
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas finibus at diam et sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam elementum finibus mi non tempor. Donec ac nulla id purus tempus mollis. Duis nec volutpat risus. Etiam pretium eleifend lacus a molestie. Donec id felis imperdiet, scelerisque augue nec, congue justo. Donec sit amet consectetur nibh, in mollis ligula.
@@ -34,6 +29,27 @@ Nunc in turpis ut diam congue malesuada. Phasellus luctus tristique lectus, id e
 <p>
 Aliquam fermentum elit massa, quis tincidunt arcu auctor et. Vestibulum vestibulum nunc a orci efficitur, in malesuada nunc placerat. Nam eleifend pharetra tortor nec mollis. Pellentesque vulputate iaculis nisi, eget facilisis ipsum interdum vitae. Aliquam laoreet, metus vel ultrices pretium, erat dui ultricies nunc, sit amet consectetur urna dui vitae purus. Cras faucibus enim et est accumsan, ac tristique enim pretium. Vestibulum sapien nisl, sodales eget posuere tempus, sollicitudin ut sapien. Curabitur pellentesque vulputate urna. In interdum quis urna eget suscipit. Vivamus sagittis nisi mi, vehicula laoreet urna auctor at. Etiam tincidunt urna sit amet consequat sagittis. Suspendisse potenti.
 </p>
+
 <img src="contact.png" alt="contact">
+
+<?php
+$to = $_POST[email];
+$subject = "Contact";
+$txt = "Bedankt voor het contact opnemen $_POST[name]! We zullen contact met u opnemen op $_POST[email].";
+$headers = "From: webmaster@example.com" . "\r\n" .
+
+
+mail($to,$subject,$txt,$headers);
+?> 
+
+ <?php
+$myfile = fopen("$_POST[name].txt", "w") or die("Unable to open file!");
+$txt = "$_POST[email] is de mail van ";
+fwrite($myfile, $txt);
+$txt = "$_POST[name]";
+fwrite($myfile, $txt);
+fclose($myfile);
+?> 
+
 </body>
-</html>
+</html> 
